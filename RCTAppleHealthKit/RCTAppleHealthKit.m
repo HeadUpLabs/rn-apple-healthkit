673be58748f4019f1edfa5f90abb57a22c1bf6c6
James Devlin <jamesd@headuplabs.com>
2018-11-30 16:42:19 +1100

    #162297852: capture apple exercise time

--- RCTAppleHealthKit/RCTAppleHealthKit.m
+++ RCTAppleHealthKit/RCTAppleHealthKit.m
@@ -182,6 +182,11 @@ RCT_EXPORT_METHOD(getBasalEnergyBurned:(NSDictionary *)input callback:(RCTRespon
     [self activity_getBasalEnergyBurned:input callback:callback];
 }
 
+RCT_EXPORT_METHOD(getAppleExerciseTime:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
+{
+    [self activity_getAppleExerciseTime:input callback:callback];
+}
+
 RCT_EXPORT_METHOD(getBodyTemperatureSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
 {
     [self vitals_getBodyTemperatureSamples:input callback:callback];
